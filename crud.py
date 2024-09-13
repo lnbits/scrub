24b6230d6f44fd3aa6523eb1d6b482156b190d0d
dni ⚡ <office@dnilabs.com>
2024-09-13 15:39:11 +0200

    feat: update to lnbits 1.0.0

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
-from typing import List, Optional, Union
+from typing import Optional, Union
 
 from lnbits.db import Database
-from lnbits.helpers import urlsafe_short_hash
+from lnbits.helpers import insert_query, update_query, urlsafe_short_hash
 
 from .models import CreateScrubLink, ScrubLink
 
@@ -10,73 +10,53 @@ db = Database("ext_scrub")
 
 async def create_scrub_link(data: CreateScrubLink) -> ScrubLink:
     scrub_id = urlsafe_short_hash()
+    scrub = ScrubLink(id=scrub_id, **data.dict())
     await db.execute(
-        """
-        INSERT INTO scrub.scrub_links (
-            id,
-            wallet,
-            description,
-            payoraddress
-        )
-        VALUES (?, ?, ?, ?)
-        """,
-        (
-            scrub_id,
-            data.wallet,
-            data.description,
-            data.payoraddress,
-        ),
+        insert_query("scrub_links", scrub),
+        scrub.dict(),
     )
-    link = await get_scrub_link(scrub_id)
-    assert link, "Newly created link couldn't be retrieved"
-    return link
+    return scrub
 
 
 async def get_scrub_link(link_id: str) -> Optional[ScrubLink]:
-    row = await db.fetchone("SELECT * FROM scrub.scrub_links WHERE id = ?", (link_id,))
+    row = await db.fetchone(
+        "SELECT * FROM scrub.scrub_links WHERE id = :id", {"id": link_id}
+    )
     return ScrubLink(**row) if row else None
 
 
-async def get_scrub_links(wallet_ids: Union[str, List[str]]) -> List[ScrubLink]:
+async def get_scrub_links(wallet_ids: Union[str, list[str]]) -> list[ScrubLink]:
     if isinstance(wallet_ids, str):
         wallet_ids = [wallet_ids]
 
-    q = ",".join(["?"] * len(wallet_ids))
-    rows = await db.fetchall(
-        f"""
-        SELECT * FROM scrub.scrub_links WHERE wallet IN ({q})
-        ORDER BY id
-        """,
-        (*wallet_ids,),
-    )
+    q = ",".join([f"'{wallet_id}'" for wallet_id in wallet_ids])
+    rows = await db.fetchall(f"SELECT * FROM scrub.scrub_links WHERE wallet IN ({q})")
     return [ScrubLink(**row) for row in rows]
 
 
-async def update_scrub_link(link_id: int, **kwargs) -> Optional[ScrubLink]:
-    q = ", ".join([f"{field[0]} = ?" for field in kwargs.items()])
+async def update_scrub_link(link: ScrubLink) -> ScrubLink:
     await db.execute(
-        f"UPDATE scrub.scrub_links SET {q} WHERE id = ?",
-        (*kwargs.values(), link_id),
+        update_query("scrub_links", link),
+        link.dict(),
     )
-    row = await db.fetchone("SELECT * FROM scrub.scrub_links WHERE id = ?", (link_id,))
-    return ScrubLink(**row) if row else None
+    return link
 
 
-async def delete_scrub_link(link_id: int) -> None:
-    await db.execute("DELETE FROM scrub.scrub_links WHERE id = ?", (link_id,))
+async def delete_scrub_link(link_id: str) -> None:
+    await db.execute("DELETE FROM scrub.scrub_links WHERE id = :id", {"id": link_id})
 
 
 async def get_scrub_by_wallet(wallet_id) -> Optional[ScrubLink]:
     row = await db.fetchone(
-        "SELECT * from scrub.scrub_links WHERE wallet = ?",
-        (wallet_id,),
+        "SELECT * from scrub.scrub_links WHERE wallet = :wallet",
+        {"wallet": wallet_id},
     )
     return ScrubLink(**row) if row else None
 
 
 async def unique_scrubed_wallet(wallet_id):
     (row,) = await db.fetchone(
-        "SELECT COUNT(wallet) FROM scrub.scrub_links WHERE wallet = ?",
-        (wallet_id,),
+        "SELECT COUNT(wallet) FROM scrub.scrub_links WHERE wallet = :wallet",
+        {"wallet": wallet_id},
     )
     return row
